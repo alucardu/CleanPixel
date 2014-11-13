3fb0335485f38254939252553aaa2aceb7e1f58d
Peter Boomsma <peter@no-illusions.nl>
2014-11-13 18:03:05 +0100

    another cpai fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,7 +43,6 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # set :keep_releases, 5
 
 namespace :deploy do
-
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
@@ -53,4 +52,17 @@ namespace :deploy do
 
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
-end
+  namespace :assets do
+    desc 'Run the precompile task locally and rsync with shared'
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        %x{bundle exec rake assets:precompile}
+        %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{host}:#{shared_path}}
+        %x{bundle exec rake assets:clean}
+      else
+        logger.info 'Skipping asset pre-compilation because there were no asset changes'
+      end
+    end
+  end
+end 
